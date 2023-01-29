58cdcc614c6c91c066c1ceca0d684ded97d75be7
v1centebarros <vmabarros@gmail.com>
2023-01-29 01:15:02 +0000

    Changed default port

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from src.player import Player
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', type=str, default='localhost')
-    parser.add_argument('--port', type=int, default=9000)
+    parser.add_argument('--port', type=int, default=9090)
     parser.add_argument('--name', type=str, default='player')
     parser.add_argument('--rsa_cheat', type=int, default=0)
     parser.add_argument('--aes_cheat', type=int, default=0)
--- playing_area.py
+++ playing_area.py
@@ -5,7 +5,7 @@ from src.playing_area import PlayingArea
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', type=str, default='localhost')
-    parser.add_argument('--port', type=int, default=9000)
+    parser.add_argument('--port', type=int, default=9090)
     args = parser.parse_args()
 
     parea = PlayingArea(args.host, args.port)
