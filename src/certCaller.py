dd441fbf8e234cab1fe3e624872548dfd40cdd35
Gui <guicostaantunes@gmail.com>
2023-02-01 18:43:38 +0000

    certCaller

---
+++ src/certCaller.py
@@ -0,0 +1,26 @@
+from utils.Auth import CC
+import os
+import click
+
+#  GUARDAR O CERTIFICADO DE UM CALLER NUMA Pasta
+def save_cert(cert, path):
+    with open(path, "wb") as f:
+        f.write(cert)
+
+#  VERFICAR SE O CERFICADO ESTA NA PASTA
+
+def check_cert(path):
+    return os.path.isfile(path)
+
+@click.option('--pin', '-p', help='Pin of the cc')
+def main(pin):
+    if pin is None:
+        print("Pin is needed")
+        return
+    cc = CC(pin)
+    cert = cc.get_cc_cert()
+    save_cert(cert, "certs/caller_cert.pem")
+    print(check_cert("certs/caller_cert.pem"))
+
+if __name__ == "__main__":
+    main()
