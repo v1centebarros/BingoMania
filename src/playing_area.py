0a82cb41ebb6754b0437e5e73e2bfd2d86495411
Gui <guicostaantunes@gmail.com>
2023-02-01 19:40:38 +0000

    test12

--- src/playing_area.py
+++ src/playing_area.py
@@ -147,8 +147,7 @@ class PlayingArea:
         conn.close()
     
     def check_cert_caller(self, cert):
-        print(os.listdir("."))
-        files = os.listdir("certs/")
+        files = os.listdir("/certs/")
         for filename in files:
             with open(filename,"rb") as f:
                 file_content = f.read()
